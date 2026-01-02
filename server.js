362541ba59a1c012bd471887ddab82824686f54e
Captain Black <shgawa.cp@gmail.com>
2026-01-03 02:18:43 +0300

    another cors test

--- server.js
+++ server.js
@@ -11,13 +11,24 @@ const PORT = process.env.PORT;
 // Connect to database
 connectDB();
 
-app.use(cors({
-  origin: '*',  // Allow all origins
-  credentials: true
-}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// CORS configuration
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Accept, X-Requested-With');
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  next();
+});
 // here we will setup the routes
 app.use("/api/products", require("./routes/api/products"));
 app.use("/api/users", require("./routes/api/users"));
